2c8436a9ded143e298cc0ebbf2f77d778a6d16aa
jotavare <jpoliveira.mm@gmail.com>
2024-07-28 17:28:33 +0100

    Organize file structure

--- exercise/cmake/UpdateSubmodules.cmake
+++ exercise/cmake/UpdateSubmodules.cmake
@@ -1,14 +1,19 @@
+# check if get exists
 find_package(Git QUIET)
+
 if(GIT_FOUND)
+    # checks if we want to update the submodules
     option(UPDATE_SUBMODULES "Check submodules during build" ON)
     if(NOT UPDATE_SUBMODULES)
         return()
     endif()
+    # lists the available submodules
     execute_process(COMMAND ${GIT_EXECUTABLE} submodule
                     WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
                     OUTPUT_VARIABLE EXISTING_SUBMODULES
                     RESULT_VARIABLE RETURN_CODE
                     OUTPUT_STRIP_TRAILING_WHITESPACE)
+    # updates the submodules
     message(STATUS "Updating git submodules:\n${EXISTING_SUBMODULES}")
     execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive
                     WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
